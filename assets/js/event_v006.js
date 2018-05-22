9ae122a139d75661513a688e78087250ed56ba98
Miao <j_j7920@yahoo.com.tw>
2018-05-22 14:39:01 +0800

    upload

---
+++ assets/js/event_v006.js
@@ -0,0 +1,174 @@
+$(function()
+{
+    var getCookie = function(strName)
+    {
+        var strNameEQ = strName + "=" ;
+        var ca = document.cookie.split(';') ;
+
+        for(var i=0; i < ca.length; i++)
+        {
+            var c = ca[i] ;
+            c = c.replace(/^\s+/, '') ;
+
+            if (c.indexOf(strNameEQ) == 0)
+            {
+                return c.substring(strNameEQ.length, c.length) ;
+            }
+        }
+
+        return null;
+    }
+
+    var getParameterByName = function(strName, strUrl)
+    {
+        if (!strUrl)
+        {
+            return null ;
+        }
+
+        strName = strName.replace(/[\[\]]/g, "\\$&");
+
+        var regex    = new RegExp("[?&]" + strName + "(=([^&#]*)|&|#|$)") ;
+        var arrMatch = regex.exec(strUrl) ;
+
+        if (!arrMatch)
+        {
+            return null ;
+        }
+
+        return !arrMatch[2] ? '' : decodeURIComponent(arrMatch[2].replace(/\+/g, " ")) ;
+    }
+
+    var strRedirect = $.trim(getParameterByName('redirect', window.location.toString())) ;
+
+    if (strRedirect == '1')
+    {
+
+        // open modal
+        $('#reconfirm').modal('show');
+
+    }
+
+    $(document)
+        .on('click', '.btn_box .activateCard', function(e)
+        {
+            var _this = $(this) ;
+
+            var _dfdLogin  = $.Deferred() ;
+
+            if (strRedirect != '1')
+            {
+                $.getJSON('//ecapi.pchome.com.tw/member/v2/member/islogin?_callback=?')
+                    .done(function(isLogin)
+                    {
+                        if (isLogin != 1)
+                        {
+                            _dfdLogin.reject() ;
+
+                            if (window.location.search == '')
+                            {
+                                location.href = 'https://ecvip.pchome.com.tw/login/v2/login.htm?rurl='+encodeURIComponent(window.location.toString() + '?redirect=1') ;
+                            }
+                            else
+                            {
+                                location.href = 'https://ecvip.pchome.com.tw/login/v2/login.htm?rurl='+encodeURIComponent(window.location.toString() + '&redirect=1') ;
+                            }
+                        }
+                        else
+                        {
+                            // 已登入導到花旗
+                            $('#sendto').modal({backdrop: 'static', keyboard: false});
+                            _dfdLogin.resolve() ;
+                        }
+                    })
+                    .fail(function()
+                    {
+                        _dfdLogin.reject() ;
+
+                        if (window.location.search == '')
+                        {
+                            location.href = 'https://ecvip.pchome.com.tw/login/v2/login.htm?rurl='+encodeURIComponent(window.location.toString() + '?redirect=1') ;
+                        }
+                        else
+                        {
+                            location.href = 'https://ecvip.pchome.com.tw/login/v2/login.htm?rurl='+encodeURIComponent(window.location.toString() + '&redirect=1') ;
+                        }
+                    }) ;
+            }
+            else
+            {
+                _dfdLogin.resolve() ;
+            }
+
+            _dfdLogin
+                .done(function()
+                {
+                    var strMemberId  = $.trim(getCookie('MBR')) ;
+
+                    if ($(".header_mobile .arrow:visible").length == 0) // pc
+                    {
+                        var isAllow = $('.main_content .agree').find('input[type="checkbox"]').prop('checked') ? 1 : 0 ;
+                    }
+                    else // Mobile
+                    {
+                        var isAllow = $('#applyCard .agree').find('input[type="checkbox"]').prop('checked') ? 1 : 0 ;
+                    }
+
+                    try
+                    {
+                        $.ajax({
+                             url : '//ecapi.pchome.com.tw/cobrand/v1/citi/apply?cusid=' + strMemberId
+                            ,type : 'POST'
+                            ,data : JSON.stringify({isAllow:isAllow})
+                            ,xhrFields: {withCredentials:true}
+                        })
+                        .done(function(objData)
+                        {
+                            if (typeof objData.ProspectId != 'undefined' && objData.ProspectId != '')
+                            {
+                                $('input[name="prospect_id"]').val(objData.ProspectId) ;
+                                $('input[name="time"]').val(objData.Time) ;
+                                $('input[name="hash"]').val(objData.Hash) ;
+                                $('#postCiti').click() ;
+                            }
+                            else
+                            {
+                                location.href = 'https://www.citibank.com.tw/credit-cards/application/index.html?CardType=15&Media=381' ;
+                            }
+                        })
+                        .fail(function(XHR)
+                        {
+                            if (XHR.status == 403)
+                            {
+                                location.href = 'https://www.citibank.com.tw/credit-cards/application/index.html?CardType=15&Media=381' ;
+                            }
+                            else
+                            {
+                                if ($.trim(XHR.responseText) != '')
+                                {
+                                    var objMsg = JSON.parse(XHR.responseText) ;
+                                    switch(objMsg.Code)
+                                    {
+                                        case '400-002':
+                                            alert('申請中，請稍後') ;
+                                        break ;
+
+                                        default:
+                                            location.href = 'https://www.citibank.com.tw/credit-cards/application/index.html?CardType=15&Media=381' ;
+                                        break ;
+                                    }
+                                }
+                                else
+                                {
+                                    location.href = 'https://www.citibank.com.tw/credit-cards/application/index.html?CardType=15&Media=381' ;
+                                }
+                            }
+                        }) ;
+                    }
+                    catch (err)
+                    {
+                        location.href = 'https://www.citibank.com.tw/credit-cards/application/index.html?CardType=15&Media=381' ;
+                    }
+                }) ;
+        }) ;
+}) ;
---
+++ assets/js/layout_V008.js
@@ -0,0 +1,175 @@
+
+var hashGo = location.hash;
+// 所有資訊載入後執行
+$('body').delay(200).queue(function (next) {
+    if (hashGo == ''){
+
+    } else {
+        $('html,body').animate({scrollTop:$(hashGo).offset().top},10);            
+    }
+});
+
+$( function(){
+
+	// Add slideDown animation to Bootstrap dropdown when expanding.
+	$('.dropdown').on('show.bs.dropdown', function() {
+		$(this).find('.dropdown-menu').first().stop(true, true).slideDown(200);
+	});
+
+	// Add slideUp animation to Bootstrap dropdown when collapsing.
+	$('.dropdown').on('hide.bs.dropdown', function() {
+		$(this).find('.dropdown-menu').first().stop(true, true).slideUp(200);
+	});
+
+	// 首頁輪播
+	$('.slider_wrap .owl-carousel, .mobile_slider .owl-carousel').owlCarousel({
+	    loop: true,
+	    margin: 10,
+	    autoplay: true,
+	    autoplayTimeout: 3500,
+	    nav: true,
+	    navText : ["<",">"],
+	    items: 1
+	});
+
+	// 品牌輪播
+	$('.store_discount .owl-carousel').owlCarousel({
+	    loop: true,
+	    margin: 10,
+	    autoplay: true,
+	    autoplayTimeout: 3500,
+	    nav: true,
+	    navText : ["<",">"],
+	    items: 1
+	});
+
+
+	// 影片輪播
+	$('.new_event .owl-carousel').owlCarousel({
+	    loop: true,
+	    merge: true,
+	    navText : ["<",">"],
+	    items:1,
+	margin:5,
+	stagePadding:0,
+	    
+        video:true,
+        lazyLoad:true,
+ 
+	    responsive:{
+	        0:{
+	            
+	        
+	        },
+	        768:{
+	         items:4  
+	        
+	        }
+	    }
+	});
+
+	// 注意事項
+	$('a.note').click(function(){
+		$('.notice').slideToggle(250);
+		$('a.note span.arw').toggleClass('rotate');
+	});
+
+
+	// 裝置判斷 PC or Mobile
+    var isMobile = {
+        iMob: function() {
+            return navigator.userAgent.match(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i);
+        }
+    };
+
+	// 連結滑動效果，PC,Mobile不同定位
+	$.scrollTo = $.fn.scrollTo = function(x, y, options){
+	    if (!(this instanceof $)) return $.fn.scrollTo.apply($('html, body'), arguments);
+
+	    // 判斷 Mobile or PC 取得y值高度
+	    if( isMobile.iMob() ) {
+	    	// Mobile
+	    	var mobTopmenu = -($('.header_desktop').outerHeight() - 270);
+	    } else {
+	    	// PC
+	    	var mobTopmenu = -($('.header_desktop').outerHeight() - -10);
+	    };
+
+	    options = $.extend({}, {
+	        gap: {
+	            x: 0,
+	            y: mobTopmenu
+	        },
+	        animation: {	// 滑動效果設定
+	            easing: 'swing',
+	            duration: 800,
+	            complete: $.noop,
+	            step: $.noop
+	        }
+	    }, options);
+
+	    return this.each(function(){
+	        var elem = $(this);
+	        elem.stop().animate({
+	            scrollLeft: !isNaN(Number(x)) ? x : $(y).offset().left + options.gap.x,
+	            scrollTop: !isNaN(Number(y)) ? y : $(y).offset().top + options.gap.y
+	        }, options.animation);
+	    });
+	};
+
+
+    $('.right_post a.card, .header_mobile li.card a, .header_mobile li.card a.btn').click(function(evn){
+        evn.preventDefault();
+        $('html,body').scrollTo(this.hash, this.hash); 
+    });
+
+    $('.fixed_under .close_under .btn').click(function(evn){
+    	$('.under_wrap').addClass('hide');
+    });
+
+	$(document).on('scroll touchmove', function() {
+		var $win = $(window),
+			w = $win.width(),
+			h = $win.height();
+
+		// 手機置頂選單
+		if ( $(window).scrollTop() <= 30 ) {
+			$('.nav_mobile').removeClass('fixed');
+			$('.main_content').removeClass('fixPadding');
+		}else {
+			$('.nav_mobile').addClass('fixed');
+			$('.main_content').addClass('fixPadding');
+		}
+
+	    // 判斷 Mobile or PC 辦卡置底顯示
+	    if( isMobile.iMob() ) {
+	    	// Mobile
+			if( $(window).scrollTop() <= 30 ) {
+				$('.fixed_under').fadeOut(300);
+			} else {
+				$('.fixed_under').fadeIn(300);
+				
+				if( ($(window).scrollTop() + h) > $('.applyCard .btn_box').position().top)
+					$('.fixed_under').addClass('hide');
+				else 
+					$('.fixed_under').removeClass('hide');
+			}
+	    } else {
+	    	// PC
+			if( $(window).scrollTop() < $('.slider_wrap, .onlyKV_01, .onlyKV_02, .onlyKV_03, .onlyKV_04').height()*.05 ){
+				$('.fixed_under').fadeOut();
+			} else {
+				$('.fixed_under').fadeIn();
+				
+				if( ($(window).scrollTop() + h) > $('.applyCard').position().top)
+					$('.fixed_under').addClass('hide');
+				else 
+					$('.fixed_under').removeClass('hide');
+			}
+	    };
+
+	}).scroll();
+});
+
+
+
